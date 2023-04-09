4c07ba14abcf56ce1d9dd2ed66d28b81ea04c504
artem-telnov <telnowtema@mail.ru>
2023-04-09 20:36:54 +0300

    Add model book-store;

--- book_store/stores/models.py
+++ book_store/stores/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+from books.models import Book
+
 
 class Store(models.Model):
     name = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updateed_at = models.DateTimeField(auto_now=True)
+
+
+class BookStore(models.Model):
+    book = models.ForeignKey(to=Book, on_delete=models.CASCADE)
+    store = models.ForeignKey(to=Store, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
